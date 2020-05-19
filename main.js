873ef1e63ea9b1d4dc519690685a890eaef84564
Ana D <annettdd@gmail.com>
2020-05-19 16:27:05 +0200

    candeleteoneflower

--- main.js
+++ main.js
@@ -1,47 +1,14 @@
-var girlLeft = 0;
-// width = 90;
-// speed = 20;
-document.addEventListener("keydown", function(event){
-    switch (event.key.toLowerCase()) {
-        case "d":
-             if ((girl.offsetLeft + girl.width +10) < window.innerWidth ) {
-               girl.style.left = `${girl.offsetLeft + 10}px`; 
-            } 
-            break;
-        case "a":
-            // if ((girl.offsetLeft + girl.width + 10) < window.innerWidth ) {
-            //     girl.style.left = `${girl.offsetLeft - 10}px`; 
-            //  } 
-           if ((girl.offsetLeft < window.innerWidth))
-            girl.style.left = `${girl.offsetLeft - 10}px`;
-            break;
-        case "w":
-            if ((girl.offsetTop + girl.width +10) < (window.innerHeight / 2)) {
-                girl.style.top = `${girl.offsetTop - 10}px`; 
-             } 
-            //girl.style.top = `${girl.offsetTop - 10}px`;
-            break;
-        case "s":
-            if ((girl.offsetTop + girl.width +10) < window.innerHeight ) {
-                girl.style.top = `${girl.offsetTop + 10}px`; 
-             } 
-            //girl.style.top = `${girl.offsetTop + 10}px`;
-            break;
-        default:
-            break;
-    }
-})
+//var girlLeft = 0;
 //making the girl
 let body = document.querySelector("body");
     let girl = document.createElement("img");
     girl.src = "./images/fita.png";
     girl.setAttribute("class", "girl");
     body.appendChild(girl);
-  
-    
+
 //making flowers after push to button
 let $body = document.querySelector("body");
-    let flowers = [];
+    //let flowers = [];
 document.getElementById("myBtn").addEventListener("click", function() {
     //setInterval(()=> {   
         let flower = document.createElement("img");
@@ -77,6 +44,78 @@ document.getElementById("myBtn").addEventListener("click", function() {
 })
 
 
+ //let flower  = document.querySelector(".flower");
+// let flower5  = document.querySelector(".flower5")
+
+function collisionDetectIon($dom1,$dom2){
+    let sq1 = {
+        x: $dom1.offsetLeft,
+        y: $dom1.offsetTop,
+        width: $dom1.offsetWidth,
+        height: $dom1.offsetHeight
+    }
+    
+    let sq2 = {
+        x:$dom2.offsetLeft,
+        y:$dom2.offsetTop,
+        width:$dom2.offsetWidth,
+        height:$dom2.offsetHeight
+    }
+    // Method 1. Works with all rectangles and is cleaner.
+    if(!(sq2.y + sq2.height < sq1.y || 
+          sq2.y > sq1.y + sq1.height ||
+          sq2.x + sq2.width < sq1.x ||
+          sq2.x > sq1.x + sq1.width   
+    )) {
+        console.log("Collision!");
+        return true;
+    } else {
+        console.log("No collision!");
+        return false
+    }
+}
+
+
+document.addEventListener("keydown", function(event){
+    let flower  = document.querySelector(".flower");
+   //added here the flower so it can be vizible to the window
+    switch (event.key.toLowerCase()) {
+        case "d":
+             if ((girl.offsetLeft + girl.width +10) < window.innerWidth ) {
+               girl.style.left = `${girl.offsetLeft + 10}px`; 
+            }           
+            break;
+        case "a":
+            //if ((girl.offsetLeft + girl.width + 10) < window.innerWidth ) {
+                girl.style.left = `${girl.offsetLeft - 10}px`; 
+            // }           
+            break;
+        case "w":
+            if ((girl.offsetTop + girl.width +10) < (window.innerHeight / 2)) {
+                girl.style.top = `${girl.offsetTop - 10}px`; 
+             } 
+            //girl.style.top = `${girl.offsetTop - 10}px`;
+            break;
+        case "s":
+            if ((girl.offsetTop + girl.width +10) < window.innerHeight ) {
+                girl.style.top = `${girl.offsetTop + 10}px`; 
+             } 
+            //girl.style.top = `${girl.offsetTop + 10}px`;
+            break;
+        default:
+            break;
+    }
+    
+        if(collisionDetectIon(girl, flower)) {
+            var elem = document.querySelector('.flower');
+              elem.parentNode.removeChild(elem);
+    }
+  
+ 
+})
+
+  
+    
 
 
 
